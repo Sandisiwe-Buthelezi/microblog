3b3bae9c445d565fd15e4f7dcead58044da7cc9f
Sandisiwe Buthelezi <siwegugu@gmail.com>
2024-01-06 21:58:19 +0200

    Added Database
    Initialized database structures(schema) and created database relationships

---
+++ app/forms.py
@@ -0,0 +1,11 @@
+#Python  module to store my web form classes
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
