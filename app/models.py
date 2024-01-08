8d598e8f7962dd3493425358ee698cfc6ee01845
Sandisiwe Buthelezi <siwegugu@gmail.com>
2024-01-08 14:17:24 +0200

    Adding User Profile page to the application
    The profile page has the following functionalities:
    Avatar image,
    Last seen,
    About me section, and
    Profile Editor

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from werkzeug.security import generate_paswowrd_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
@@ -6,6 +6,7 @@ import sqlalchemy.orm as so
 from app import db
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -17,15 +18,26 @@ class User(UserMixin, db.Model):
 
     posts: so.WriteOnlyMapped['Post'] = so.relationship(
         back_populates='author')
+
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc))
     
     def set_password(self, password):
-        self.password_hash = generate_paswowrd_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def chech_password(self, password):
         return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+
+    
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
